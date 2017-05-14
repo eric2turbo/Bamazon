3d5ede1623497ce5e3d31b3129065079e8d94afc
Eric <eric@someone.com>
2017-05-13 18:48:07 -0700

    supervisor functions added

--- BamazonSupervisor.js
+++ BamazonSupervisor.js
@@ -19,8 +19,84 @@ connection.connect(function(err) {
     console.log('connected');
 });
 
-connection.query("SELECT * FROM departments WHERE department_name = 'Electronics'",
-    function(err, res) {
-        if (err) throw err;
-        console.log(res);
+// Allows Supervisor to select what to do
+function svDecision() {
+    inquirer.prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+            "Add Department",
+            "View Product Sales by Department",
+            "Quit"
+        ]
+    }).then(function(answer) {
+        switch (answer.action) {
+            case "Add Department":
+                addDept();
+                break;
+
+            case "View Product Sales by Department":
+                productSales();
+                break;
+
+            case "Quit":
+                exit();
+        }
     });
+}
+
+// Lists sales and profit for a department
+function productSales() {
+    connection.query("SELECT *, total_sales - over_head_costs AS total_profit FROM departments", function(err, res) {
+        var colArr = [];
+        for (var i = 0; i < res.length; i++) {
+            colArr.push({
+                department_id: res[i].department_id,
+                department_name: res[i].department_name,
+                overhead_costs: res[i].over_head_costs,
+                total_sales: res[i].total_sales,
+                total_profit: res[i].total_profit
+            });
+        }
+        var columns = columnify(colArr);
+        console.log(columns);
+        svDecision();
+    });
+}
+
+// Adds a Department to departments table
+function addDept() {
+    inquirer.prompt([{
+            name: "dept",
+            message: "Name of the Department:"
+        }, {
+            name: "overhead",
+            message: "Overhead costs of the Department:",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+    ]).then(function(answer) {
+        connection.query("INSERT INTO departments SET ?", {
+            over_head_costs: answer.overhead,
+            department_name: answer.dept,
+
+        });
+        console.log("New Department Added");
+        svDecision();
+    });
+}
+
+function exit() {
+    console.log("--------------------------------");
+    console.log("See you later");
+    console.log("--------------------------------");
+    connection.end();
+}
+
+svDecision();
